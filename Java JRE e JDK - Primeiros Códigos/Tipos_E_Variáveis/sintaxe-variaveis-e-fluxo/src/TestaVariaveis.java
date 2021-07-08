eb4e1026112d13ee79ec467fa657fac04cf9934c
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-07-08 14:12:17 -0300

    Tipos de variáveis: int (inteiro)

---
+++ Java JRE e JDK - Primeiros Códigos/Tipos_E_Variáveis/sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,16 @@
+public class TestaVariaveis {
+	public static void main(String[] args) {
+		System.out.println("Tipos e variáveis");
+		
+		int idade = 21;
+		System.out.println(idade);
+		
+		idade = 21 + 10;
+		System.out.println(idade);
+		
+		idade = (5 * 7) + 2;
+		System.out.println(idade);
+		
+		System.out.println("A idade é: " + idade);
+	}
+}
