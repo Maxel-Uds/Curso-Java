0cfc99cabd6c0aff6252beaf229e11c87f6db5ac
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-07-08 14:53:15 -0300

    Casting em java e outros tipos de variáveis

--- Java JRE e JDK - Primeiros Códigos/Tipos_E_Variáveis/sintaxe-variaveis-e-fluxo/src/TestaConversao.java
+++ Java JRE e JDK - Primeiros Códigos/Tipos_E_Variáveis/sintaxe-variaveis-e-fluxo/src/TestaConversao.java
@@ -2,7 +2,15 @@
 public class TestaConversao {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		double salario = 1270.50;
+		int valor = (int)salario;
+		System.out.println(valor);
+		
+		long numeroGrande; //Guarda valores de até 64 bits
+		short numeroPequeno; //Guarda valores de até 16 bits
+		byte b; //Vai de -128 até 127
+		float pontoFlutuante; //Guarda valores quebrados de até 32 bits
 
 	}
 
