df574b97fb6279ba16d154f0f62ae17300340bf8
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-07-12 09:21:02 -0300

    Booleanos e operadores lógicos

---
+++ Java JRE e JDK - Primeiros Códigos/Tipos_E_Variáveis/sintaxe-variaveis-e-fluxo/src/TestaConcidional2.java
@@ -0,0 +1,23 @@
+
+public class TestaConcidional2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Testando condicionais");
+		int idade = 17;
+		int quantidadePessoas = 3;
+		boolean acompanhado = quantidadePessoas >= 2;
+
+		if (idade >= 18 || acompanhado) {
+
+			System.out.println("Bem vindo!!");
+			
+		} else {
+							
+				System.out.println("Você é menor de idade");
+			
+		}
+
+	}
+
+}
