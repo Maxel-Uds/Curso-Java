1461c7ed907e85f3e2f80f5fa2a0ad29b9c6ca4e
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-07-12 09:30:25 -0300

    Escopo e inicialização de variáveis

---
+++ Java JRE e JDK - Primeiros Códigos/Tipos_E_Variáveis/sintaxe-variaveis-e-fluxo/src/TestaEscopo.java
@@ -0,0 +1,33 @@
+public class TestaEscopo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Testando condicionais");
+		int idade = 17;
+		int quantidadePessoas = 3;
+		//boolean acompanhado = quantidadePessoas >= 2;
+		boolean acompanhado;
+		
+		if(quantidadePessoas >= 2) {
+			
+			acompanhado = true;
+			
+		} else {
+			
+			acompanhado = false;
+			
+		}
+
+		if (idade >= 18 || acompanhado) {
+
+			System.out.println("Bem vindo!!");
+			
+		} else {
+							
+				System.out.println("Você é menor de idade");
+			
+		}
+
+	}
+
+}
