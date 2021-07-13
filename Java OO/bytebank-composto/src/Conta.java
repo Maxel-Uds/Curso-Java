813e22cbf597bd6a9e8ea3765c76e7c9b5f0473f
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-07-13 08:45:52 -0300

    Composição de objetos

---
+++ Java OO/bytebank-composto/src/Conta.java
@@ -0,0 +1,45 @@
+public class Conta {
+	
+	double saldo;
+	int agencia;
+	int numero;
+	Cliente titular;
+	
+	public void deposita(double valor) {
+		
+		saldo += valor;
+		
+	}
+	
+	public boolean saca(double valor) {
+		
+		if(saldo >= valor) {
+			
+			saldo -= valor;
+			return true;
+			
+		} else {
+			
+			return false;
+			
+		}
+		
+	}
+	
+	public boolean transfere(double valor, Conta conta) {
+		
+		if(saldo >= valor) {
+			
+			this.saca(valor);
+			conta.deposita(valor);
+			return true;
+			
+		} else {
+			
+			return false;
+			
+		}
+		
+	}
+	
+}
