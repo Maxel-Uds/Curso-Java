b81560c1c731d9db8a1478ad9833e8d1fabcb8bb
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-07-14 16:59:36 -0300

    Mais uma classe abstrata

---
+++ Java Polimorfismo - herança e interfaces/bytebank-herdado/src/FuncionarioAutenticavel.java
@@ -0,0 +1,14 @@
+
+public abstract class FuncionarioAutenticavel extends Funcionario {
+
+	private int senha;
+	
+	public boolean autentica(int senha) {
+		if(this.senha == senha) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
--- Java Polimorfismo - herança e interfaces/bytebank-herdado/src/Gerente.java
+++ Java Polimorfismo - herança e interfaces/bytebank-herdado/src/Gerente.java
@@ -1,15 +1,7 @@
 
-public class Gerente extends Funcionario {
+public class Gerente extends FuncionarioAutenticavel {
 
 	private int senha;
-	
-	public boolean autentica(int senha) {
-		if(this.senha == senha) {
-			return true;
-		} else {
-			return false;
-		}
-	}
 	
 	public double getBonificacao() {
 		return + super.getSalario();
