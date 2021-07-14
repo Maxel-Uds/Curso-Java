f6a149c770ed791c7103a5495cda323f8f7fa54a
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-07-14 17:30:51 -0300

    Primeira interface em java

--- Java Polimorfismo - herança e interfaces/bytebank-herdado/src/Cliente.java
+++ Java Polimorfismo - herança e interfaces/bytebank-herdado/src/Cliente.java
@@ -1,8 +1,14 @@
-
-public abstract class FuncionarioAutenticavel extends Funcionario {
+public class Cliente implements Autenticavel {
 
 	private int senha;
 	
+	@Override
+	public void setSenha(int senha) {
+		this.senha = senha;
+		
+	}
+
+	@Override
 	public boolean autentica(int senha) {
 		if(this.senha == senha) {
 			return true;
@@ -10,5 +16,6 @@ public abstract class FuncionarioAutenticavel extends Funcionario {
 			return false;
 		}
 	}
-
+	
+	
 }
