bcece9c83bcfc80310e8fb7d3bb62c4593d791e8
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-07-20 12:54:13 -0300

    Métodos da Classe String

--- Java e Java-Lang/bytebank-herdado-conta/src/br/com/Bytebank/Banco/Teste/TesteString.java
+++ Java e Java-Lang/bytebank-herdado-conta/src/br/com/Bytebank/Banco/Teste/TesteString.java
@@ -4,12 +4,22 @@ public class TesteString {
 
 	public static void main(String[] args) {
 		
+		
+		//A String é um objeto imutável
 		String nome = "Alura"; //Object literal
+		
 		//String outro = new String("Alura");
 		
-		String outro = nome.replace("A", "a");
-		//nome.toLowerCase();
-		System.out.println(outro);
+		//String outro = nome.replace("A", "a");
+		
+		//String outro = nome.toUpperCase();
+		
+		//char c = nome.charAt(2);
+		
+		//int pos = nome.indexOf("ur");
+		
+		int tamanho = nome.length();
+		System.out.println(tamanho); 
 	}
 
 }
