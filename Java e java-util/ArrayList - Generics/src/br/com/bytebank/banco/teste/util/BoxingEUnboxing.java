8c408658c51fb6a62dd772ab3ba8db55d736fa04
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-07-22 12:45:01 -0300

    Métodos da classe Integer

--- Java e java-util/ArrayList - Generics/src/br/com/bytebank/banco/teste/util/BoxingEUnboxing.java
+++ Java e java-util/ArrayList - Generics/src/br/com/bytebank/banco/teste/util/BoxingEUnboxing.java
@@ -8,9 +8,17 @@ public class BoxingEUnboxing {
 	public static void main(String[] args) {
 		
 		int idade = 29;
-		Integer idadeRef = new Integer(idade);
+		Integer idadeRef = Integer.valueOf(idade);//Autoboxing
+		System.out.println(idadeRef.intValue());//Unboxing
+		System.out.println(idadeRef.doubleValue());
+		System.out.println(Integer.MAX_VALUE);
+		
+		String s = args[0];
+//		Integer num = Integer.valueOf(s);
+		int num = Integer.parseInt(s);
+		
 		List<Integer> lista = new ArrayList<Integer>();
-		lista.add(idadeRef);
+		lista.add(29); //Autoboxing
 
 	}
 
