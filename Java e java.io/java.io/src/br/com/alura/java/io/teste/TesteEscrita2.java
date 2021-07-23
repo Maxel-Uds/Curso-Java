a2777fdca0595a6a346f65c5d55d5625d79a767a
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-07-23 14:33:57 -0300

    Saída com FileWriter

---
+++ Java e java.io/java.io/src/br/com/alura/java/io/teste/TesteEscrita2.java
@@ -0,0 +1,33 @@
+package br.com.alura.java.io.teste;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+public class TesteEscrita2 {
+
+	public static void main(String[] args) throws IOException {
+
+		//Fluxo de entrada com Arquivo	
+//		OutputStream fos = new FileOutputStream("lorem2.txt");
+//		Writer osw = new OutputStreamWriter(fos);
+//		BufferedWriter bw = new BufferedWriter(osw);
+		
+//		FileWriter fw = new FileWriter("lorem2.txt");
+		
+		BufferedWriter bw = new BufferedWriter(new FileWriter("lorem2.txt"));
+		bw.write("Suspendisse eleifend, erat et tincidunt rutrum, metus velit accumsan magna, vel semper ex mauris ac felis.");
+//		bw.write("\n");
+//		bw.write(System.lineSeparator());
+		bw.newLine();
+		bw.write("Aenean tortor justo, faucibus non nunc sed, molestie tempus nunc.");
+		
+		bw.close();
+		
+	}
+
+}
