a35eb9bb07257c558e70aa956274f0c989f84cf7
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-07-23 12:59:15 -0300

    Outras entradas e saídas

---
+++ Java e java.io/java.io/src/br/com/alura/java/io/teste/TesteCopiarArquivo.java
@@ -0,0 +1,44 @@
+package br.com.alura.java.io.teste;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.Socket;
+
+public class TesteCopiarArquivo {
+
+	public static void main(String[] args) throws IOException {
+
+		Socket s = new Socket();
+		
+		//Fluxo de entrada com Arquivo	
+		InputStream fis = s.getInputStream(); //System.in; //new FileInputStream("lorem.txt");
+		InputStreamReader isr = new InputStreamReader(fis);
+		BufferedReader br = new BufferedReader(isr);
+		
+		//Fluxo de saída
+		OutputStream fos = s.getOutputStream(); //System.out; //new FileOutputStream("textoDoTeclado.txt");
+		Writer osw = new OutputStreamWriter(fos);
+		BufferedWriter bw = new BufferedWriter(osw);
+		
+		String linha = br.readLine();
+		
+		while(linha != null && !linha.isEmpty()) {
+			bw.write(linha);
+			bw.newLine();
+			linha = br.readLine();
+		}
+		
+		br.close();
+		bw.close();
+		
+	}
+
+}
