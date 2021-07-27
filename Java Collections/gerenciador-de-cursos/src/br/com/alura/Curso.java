2a5a07e0e95ab45146b64d370def60a13c0426ea
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-07-27 13:33:48 -0300

    Aplicando o set no modelo

--- Java Collections/gerenciador-de-cursos/src/br/com/alura/Curso.java
+++ Java Collections/gerenciador-de-cursos/src/br/com/alura/Curso.java
@@ -2,13 +2,16 @@ package br.com.alura;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Curso {
 
 	private String nome;
 	private String instrutor;
 	private List<Aula> aulas = new ArrayList<Aula>();
+	private Set<Aluno> alunos = new HashSet<Aluno>();
 	
 	public Curso(String nome, String instrutor) {
 		this.nome = nome;
@@ -38,6 +41,14 @@ public class Curso {
 	@Override
 	public String toString() {
 		return "Curso: " + this.nome + ", tempo total: " + getTempoTotal();
+	}
+
+	public void matricula(Aluno aluno) {
+		this.alunos.add(aluno);
+	}
+	
+	public Set<Aluno> getAlunos() {
+		return Collections.unmodifiableSet(alunos);
 	}
 	
 }
---
+++ Java Collections/gerenciador-de-cursos/src/br/com/alura/TestaCursoComAluno.java
@@ -0,0 +1,25 @@
+package br.com.alura;
+
+public class TestaCursoComAluno {
+
+	public static void main(String[] args) {
+
+Curso javaColecoes = new Curso("Dominando as coleções", "Paulo Silveira");
+		
+		javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
+		javaColecoes.adiciona(new Aula("Criando uma aula", 20));
+		javaColecoes.adiciona(new Aula("Modelando com coleções", 22));
+		
+		Aluno a1 = new Aluno("Rodrigo Turin", 34672);
+		Aluno a2 = new Aluno("João Silva", 34673);
+		Aluno a3 = new Aluno("Jonas Augusto", 34674);
+		
+		javaColecoes.matricula(a1);
+		javaColecoes.matricula(a2);
+		javaColecoes.matricula(a3);
+		
+		javaColecoes.getAlunos().forEach(a -> System.out.println(a));
+
+	}
+
+}
