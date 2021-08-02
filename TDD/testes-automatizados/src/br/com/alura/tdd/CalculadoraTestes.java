b3c0b41b952bc508e004f47f5a1239a6c2819289
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-08-02 15:28:46 -0300

    Primeiro teste automatizado

---
+++ TDD/testes-automatizados/src/br/com/alura/tdd/CalculadoraTestes.java
@@ -0,0 +1,23 @@
+package br.com.alura.tdd;
+
+public class CalculadoraTestes {
+
+	public static void main(String[] args) {
+		
+		Calculadora calc = new Calculadora();
+		
+		int soma = calc.somar(2, 3);
+		System.out.println(soma);
+		
+		soma = calc.somar(2, 0);
+		System.out.println(soma);
+		
+		soma = calc.somar(0, 0);
+		System.out.println(soma);
+		
+		soma = calc.somar(2, -1);
+		System.out.println(soma);
+
+	}
+
+}
