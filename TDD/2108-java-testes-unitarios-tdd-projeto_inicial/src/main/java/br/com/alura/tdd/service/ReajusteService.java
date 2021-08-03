8e20ead07e9326f7114765e1621a14473641ae83
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-08-03 15:04:02 -0300

    Refatoração

--- TDD/2108-java-testes-unitarios-tdd-projeto_inicial/src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ TDD/2108-java-testes-unitarios-tdd-projeto_inicial/src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -7,16 +7,8 @@ import br.com.alura.tdd.modelo.Funcionario;
 public class ReajusteService {
 
 	public void concederReajuste(Funcionario func, Desempenho desempenho) {
-		if(desempenho == Desempenho.A_DESEJAR) {
-			BigDecimal reajuste = func.getSalario().multiply(new BigDecimal("0.03"));
+			BigDecimal reajuste = func.getSalario().multiply(desempenho.percentualReajuste());
 			func.reajustarSalario(reajuste);
-		} else if(desempenho == Desempenho.BOM) {
-			BigDecimal reajuste = func.getSalario().multiply(new BigDecimal("0.15"));
-			func.reajustarSalario(reajuste);
-		} else {
-			BigDecimal reajuste = func.getSalario().multiply(new BigDecimal("0.2"));
-			func.reajustarSalario(reajuste);
-		}
 	}
 
 }
--- TDD/2108-java-testes-unitarios-tdd-projeto_inicial/src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
+++ TDD/2108-java-testes-unitarios-tdd-projeto_inicial/src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
@@ -1,11 +1,9 @@
 package br.com.alura.tdd.service;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import org.junit.jupiter.api.Test;
-
 import br.com.alura.tdd.modelo.Desempenho;
 import br.com.alura.tdd.modelo.Funcionario;
 
