e161fe4ee276562e0054e8c2afabed338a955bdc
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-08-03 13:50:11 -0300

    Mais testes com JUnit

---
+++ TDD/2108-java-testes-unitarios-tdd-projeto_inicial/src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -0,0 +1,35 @@
+package br.com.alura.tdd.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
+import br.com.alura.tdd.modelo.Funcionario;
+
+class BonusServiceTest {
+
+	@Test
+	void bonusDeveriaSerZero() {
+		BonusService service = new BonusService();
+		BigDecimal bonus = service.calcularBonus(new Funcionario("João", LocalDate.now(), new BigDecimal("25000")));
+		
+		assertEquals(BigDecimal.ZERO, bonus);
+	}
+	
+	@Test
+	void bonusDeveriaSer10PorCento() {
+		BonusService service = new BonusService();
+		BigDecimal bonus = service.calcularBonus(new Funcionario("Lucas", LocalDate.now(), new BigDecimal("2500")));
+		
+		assertEquals(new BigDecimal("250.0"), bonus);
+	}
+	
+	@Test
+	void salarioDeDezMil() {
+		BonusService service = new BonusService();
+		BigDecimal bonus = service.calcularBonus(new Funcionario("Flávio", LocalDate.now(), new BigDecimal("10000")));
+		
+		assertEquals(new BigDecimal("1000.0"), bonus);
+	}
+
+}
