eaf997cc8366104fb53ad85fd89af20b76f4daa5
Maxel-Uds <maxellopes32@gmail.com>
2021-08-30 11:55:15 -0300

    Restringindo o acesso aos endpoints privados

--- Segurança na API Rest/src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ Segurança na API Rest/src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -2,6 +2,11 @@ package br.com.alura.forum.config.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 @Configuration
@@ -14,11 +19,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequest().antMatchers(HttpMethod.GET, "/topicos").permitAll().antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/topicos").permitAll()
+                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin();
     }
 
     @Override
-    protected void configure(WebSecurity web) throws Exception {
+    public void configure(WebSecurity web) throws Exception {
 
     }
 
