93a39f8560d001b78efe4959710963e7065aeb17
Maxel-Uds <maxellopes32@gmail.com>
2021-08-30 11:05:18 -0300

    Liberando acesso aos endpoints públicos

--- Segurança na API Rest/src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ Segurança na API Rest/src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -1,11 +1,25 @@
 package br.com.alura.forum.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequest().antMatchers(HttpMethod.GET, "/topicos").permitAll().antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+    }
+
+    @Override
+    protected void configure(WebSecurity web) throws Exception {
+
+    }
 
 }
