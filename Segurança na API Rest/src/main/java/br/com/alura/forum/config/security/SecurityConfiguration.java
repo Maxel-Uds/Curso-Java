b05396864d3accf8b241073fc1b6469535db4adb
Maxel-Uds <maxellopes32@gmail.com>
2021-08-30 10:35:00 -0300

    Habilitando o spring security

---
+++ Segurança na API Rest/src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -0,0 +1,11 @@
+package br.com.alura.forum.config.security;
+
+import org.springframework.context.annotation.Configuration;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+
+}
